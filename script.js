1b3cb07658242130e9c97e58e612066781c6fb47
zhaiiyq <akzajykp@gmail.com>
2026-02-13 14:13:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function openEnvelope() {
+    document.getElementById("scene1").classList.remove("active");
+    document.getElementById("scene2").classList.add("active");
+}
+
+function noAnswer() {
+    document.getElementById("scene2").classList.remove("active");
+    document.getElementById("scene3").classList.add("active");
+}
+
+function yesAnswer() {
+    document.getElementById("scene2").classList.remove("active");
+    document.getElementById("scene4").classList.add("active");
+}
+
+function backToQuestion() {
+    document.getElementById("scene3").classList.remove("active");
+    document.getElementById("scene2").classList.add("active");
+}
 
